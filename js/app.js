f5c89db2f498d4e791832db13742b592cc8f364a
EC2 Default User <ec2-user@ip-172-31-27-92.us-east-2.compute.internal>
2020-02-24 01:24:04 +0000

    AutomatedCommitAndPush

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ function enter_button_was_clicked(){
 }
 
 function handle_button_click(button){
-    alert(button.val());
+    alert("hello");
 }
 
-$(":button").on("click", handle_button_click($(this)));
+$(":button").on("click", handle_button_click);
