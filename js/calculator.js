d1763865844543c3e833ff956b489e6afe5d91e4
EC2 Default User <ec2-user@ip-172-31-27-92.us-east-2.compute.internal>
2020-02-24 03:51:04 +0000

    AutomatedCommitAndPush

--- js/calculator.js
+++ js/calculator.js
@@ -17,7 +17,7 @@ function append_expression(value){
 }
 
 function handle_button_click(button){
-    console.log(button);
+    console.log($(this).id);
     if ( button.id == "one" ){
         append_expression("1");
     }
