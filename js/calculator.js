931c828df63145fa094060c58d7ede53a16f8fb1
EC2 Default User <ec2-user@ip-172-31-27-92.us-east-2.compute.internal>
2020-02-24 04:30:46 +0000

    AutomatedCommitAndPush

--- js/calculator.js
+++ js/calculator.js
@@ -15,6 +15,9 @@ function evaluate_expression(){
             r = result.split('"')[1];
             $("#expression").val(r);
         }
+        error: function(status, error){
+            $("#expression").val("error");
+        }
     });
 }
 
