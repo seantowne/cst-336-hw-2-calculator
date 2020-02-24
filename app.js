173884f52e6f80627fc0326992ce270f8fba6bd9
EC2 Default User <ec2-user@ip-172-31-27-92.us-east-2.compute.internal>
2020-02-24 02:14:10 +0000

    AutomatedCommitAndPush

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.get(express.static(path.join(__dirname,"css/style.css")));
+app.use(express.static(__dirname));
 app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/index.html')));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
